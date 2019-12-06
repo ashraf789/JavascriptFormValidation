2c6ca6a0981283a1aca812acbd3082d33b8ee787
Ashraf789 <syedashrafullah15@gmail.com>
2019-12-06 17:06:22 +0700

    Confirm password validation add

--- js/app.js
+++ js/app.js
@@ -1,56 +1,70 @@
-    $(document).ready(function() {
 
-        $('input[type=password]').keyup(function() {
-                // keyup code here
-                var pswd = $(this).val();
-            //validate the length
-            if ( pswd.length < 8 ) {
-                $('#length').removeClass('valid').addClass('invalid');
-            } else {
-                $('#length').removeClass('invalid').addClass('valid');
-            }
 
-            //validate letter
-            if ( pswd.match(/[a-z]/) ) {
-                $('#letter').removeClass('invalid').addClass('valid');
-            } else {
-                $('#letter').removeClass('valid').addClass('invalid');
-            }
 
-            //validate capital letter
-            if ( pswd.match(/[A-Z]/) ) {
-                $('#capital').removeClass('invalid').addClass('valid');
-            } else {
-                $('#capital').removeClass('valid').addClass('invalid');
-            }
+function passValidation(passInputId){
 
-            //validate number
-            if ( pswd.match(/\d/) ) {
-                $('#number').removeClass('invalid').addClass('valid');
-            } else {
-                $('#number').removeClass('valid').addClass('invalid');
-            }
-
-            passwordValid();
-            
-
-        }).focus(function() {
-            passwordValid();
-        });
-
+    var pswd = $(passInputId).val();
+    //validate the length
+    if ( pswd.length < 8 ) {
+        $('#length').removeClass('valid').addClass('invalid');
+    } else {
+        $('#length').removeClass('invalid').addClass('valid');
     }
 
-
-    );
-
-    function passwordValid(){
-        var pswd = $('#password_txt').val();
-        if (pswd.length >=8 && pswd.match(/[a-z]/) && pswd.match(/[A-Z]/) && pswd.match(/\d/)) {
-            $('#pswd_info').hide();
-
-            return true;
-        }
-
-        $('#pswd_info').show();
-        return false;
+    //validate letter
+    if ( pswd.match(/[a-z]/)) {
+        $('#letter').removeClass('invalid').addClass('valid');
+    } else {
+        $('#letter').removeClass('valid').addClass('invalid');
     }
+    
+    //validate capital letter
+    if ( pswd.match(/[A-Z]/) ) {
+        $('#capital').removeClass('invalid').addClass('valid');
+    } else {
+        $('#capital').removeClass('valid').addClass('invalid');
+    }
+    // validate number
+    if ( pswd.match(/\d/) ) {
+        $('#number').removeClass('invalid').addClass('valid');
+    } else {
+        $('#number').removeClass('valid').addClass('invalid');
+    }
+
+    // Hide or show validation rule
+    isValidPass(passInputId);
+}
+
+/**
+* Hide or focus password field
+* Return boolen according to validation
+*/
+function isValidPass(passInputId){
+
+    var pswd = $(passInputId).val();
+    if (pswd.length >=8 && (pswd.match(/[a-z]/) && pswd.match(/[A-Z]/)) && pswd.match(/\d/)) {
+        $('#pswd_info').hide();
+        return true;
+    }
+
+    $('#pswd_info').show();
+    return false;
+}
+
+
+/**
+* Hide or show confirm pass not match text
+*/
+function isConfirmPassMatch(passId, confirmPassId){
+    var pswd = $(passId).val();
+    var confirmPswd = $(confirmPassId).val();
+
+    if (pswd == confirmPswd) {
+        $('#confirm_pswd_info').hide();
+        return true;
+    }
+
+    $('#confirm_pswd_info').show();
+    return false;
+
+}
